8e55ec80e5a38289306af53e7b568f21c5c4a37e
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-03-30 12:39:31 +0300

    modal_windows

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open-buynow]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      modal: document.querySelector('[data-modal-buynow]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
@@ -12,4 +12,84 @@
       refs.modal.classList.toggle('is-hidden');
       document.body.classList.toggle('overflow'); //add class and disable scroll in CSS body.overflow
     }
+  })();
+  // leave-a-review-about-our-chocolate 
+  // <button class="btn-buynow" type="button" data-modal-open-leave-a-review>
+  //       review
+  //     </button>
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-leave-a-review]'),
+      closeModalBtn: document.querySelector('[data-modal-close-leave-a-review]'),
+      modal: document.querySelector('[data-modal-leave-a-review]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      document.body.classList.toggle('overflow'); //add class and disable scroll in CSS body.overflow
+    }    
+  })();
+  // thank-you-for-your-feedback
+  // <button class="btn-buynow" type="button" data-modal-open-feedback>
+  //       feedback
+  //     </button>
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-feedback]'),
+      closeModalBtn: document.querySelector('[data-modal-close-feedback]'),
+      modal: document.querySelector('[data-modal-feedback]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      document.body.classList.toggle('overflow'); //add class and disable scroll in CSS body.overflow
+    }
+    
+  })();
+  // explore-our-new-chocolate-first.html
+  // // <button class="btn-buynow" type="button" data-modal-open-explore-our-new>
+  // explore-our-new
+  // </button>
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-explore-our-new]'),
+      closeModalBtn: document.querySelector('[data-modal-close-explore-our-new]'),
+      modal: document.querySelector('[data-modal-explore-our-new]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      document.body.classList.toggle('overflow'); //add class and disable scroll in CSS body.overflow
+    }
+    
+  })();
+  
+  // thank-you
+//  <button class="btn-buynow" type="button" data-modal-open-thank-you>
+//          thank-you
+//        </button> 
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open-thank-you]'),
+      closeModalBtn: document.querySelector('[data-modal-close-thank-you]'),
+      modal: document.querySelector('[data-modal-thank-you]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      document.body.classList.toggle('overflow'); //add class and disable scroll in CSS body.overflow
+    }
+    
   })();
