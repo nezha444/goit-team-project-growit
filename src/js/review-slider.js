eed369ab0cc0fe6386bee4d4858eed9e72993049
Tensys57 <tensys57@gmail.com>
2023-03-31 13:59:41 +0300

    Added slider

---
+++ src/js/review-slider.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  slidesToScroll: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      paceBetween: 28,
+    },
+    768: { slidesPerView: 2 },
+  },
+});
