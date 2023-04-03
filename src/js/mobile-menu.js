abb666ced9ca86b9bbff5267dd2f900b6e1db691
Nikita <bazilik2452@gmail.com>
2023-04-03 11:56:23 +0200

    fix header, add js for menu

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,6 +2,7 @@
     const mobileMenu = document.querySelector('.js-menu-container');
     const openMenuBtn = document.querySelector('.js-open-menu');
     const closeMenuBtn = document.querySelector('.js-close-menu');
+    const linksMenu = document.querySelectorAll('.menu-modal');
   
     const toggleMenu = () => {
       const isMenuOpen =
@@ -12,6 +13,7 @@
   
     openMenuBtn.addEventListener('click', toggleMenu);
     closeMenuBtn.addEventListener('click', toggleMenu);
+    linksMenu.forEach(link => link.addEventListener('click', toggleMenu))
   
     // Close the mobile menu on wider screens if the device orientation changes
     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
